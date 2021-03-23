c480392a237403b7f232ae402a7ced5ec6a71add
Akif Islam <iamakifislam@gmail.com>
2021-03-23 22:38:01 +0600

    Create OOPwithC++.cpp

---
+++ OOPwithC++.cpp
@@ -0,0 +1,131 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//Student Class
+class Subject {
+public:
+    string subject_name;
+    string Subject_Code;
+    vector<double> quiz_marks;
+    double final_marks;
+
+    Subject() {
+        final_marks = 0;
+    }
+
+    // Subject Name, Quiz Calculation
+    void subject_info() {
+        printf("Enter Subject Name :");
+        cin >> subject_name;
+
+        printf("Enter Subject Code :");
+        cin >> Subject_Code;
+
+        int no_of_quiz, quiz_mark;
+        printf("Enter No of Quiz :");
+        cin >> no_of_quiz;
+
+        for (int i = 0; i < no_of_quiz; i++) {
+            cin >> quiz_mark;
+            quiz_marks.push_back(quiz_mark);
+            final_marks += quiz_mark;
+        }
+//            print_marks();
+    }
+
+    void print_marks() {
+        cout << "Subject Name : " << subject_name << endl;
+        cout << "Subject Code : " << Subject_Code << endl;
+        cout << "=== QUIZ MARKS of " << subject_name << "===" << endl;
+        for (int i = 0; i < quiz_marks.size(); i++) {
+            printf("Quiz %d : %.2lf\t", i + 1, quiz_marks[i]);
+        }
+        cout << endl;
+        cout << "== == == == ==" << endl;
+        cout << "Total Marks from Course " << subject_name << " is : " << final_marks << endl;
+        cout << endl;
+
+    }
+
+
+};
+
+
+class Student {
+
+
+    //Member
+private :
+    string student_name;
+    int roll_number;
+    double final_marks;
+    int no_of_subject;
+    Subject *course;
+
+
+public :
+    //Constructor
+    Student() {
+        //This funciton will take input of Student's Name and Roll Number Only
+        user_basic_info();
+    }
+
+public:
+
+    void user_basic_info() {
+
+        printf("Enter Student Name :\n");
+        getline(cin, student_name);
+        printf("Enter Student Roll Number :\n");
+        cin >> roll_number;
+
+        printf("Enter the Number of Subject: \n");
+
+
+        cin >> no_of_subject;
+        course = new Subject[no_of_subject];
+
+
+        for (int i = 0; i < no_of_subject; i++) {
+            course[i].subject_info();
+        }
+        printAll();
+
+
+    }
+
+    void printAll() {
+        cout << "Student Name : " << student_name << endl;
+        cout << "Roll : " << roll_number << endl;
+        cout << endl;
+        cout << endl;
+        cout << endl;
+
+        for (int i = 0; i < no_of_subject; i++) {
+            course[i].print_marks();
+        }
+    }
+
+    void DestroyEverything() {
+        free(course);
+    }
+
+};
+
+int main() {
+    Student *s;
+    s = new Student();
+    cout<<"Deleting All References"<<endl;
+    free(s);
+    s = nullptr;
+
+    cout<<"Freed"<<endl;
+
+}
+
+/*
+ *
+
+
+ */
